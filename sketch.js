794f94bfaddb4fb3e67798881b0238950f186584
Reian Beltran <rmb20j@fsu.edu>
2022-10-18 21:38:59 -0400

    updated animation, added another yellow circle

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 let d = 250;
-let dtwo = d/2;
+let dtwo = 125;
+let dthree = 125;
 function setup() {
   createCanvas(500, 500);
 }
@@ -12,11 +13,12 @@ function draw() {
   circle(250, 0, d);
   circle(250, 500, d);
   circle(500, 250, d);
-  d++;
+  d--;
 
-  if (d > 500) {
+  if (d < 80) {
     d = 250;
   }
+
 
   fill(119, 0, 200);
   noStroke;
@@ -25,4 +27,24 @@ function draw() {
   circle(375, 375, dtwo);
   circle(500/4, 500/4, dtwo);
   circle(375, 500/4, dtwo);
+  
+  dtwo++;
+
+  if (dtwo > 350) {
+    dtwo = 125;
+  }
+
+  if (d < 250)
+  {
+    fill(252, 266, 5);
+    noStroke;
+    circle(250, 250, dthree);
+    dthree++;
+
+      if (dthree > 350)
+      {
+        dthree = 125;
+      }
+    
+  }
 }
