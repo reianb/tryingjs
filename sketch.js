a66d33eb07b96f6279292cf3503879fdb6cb3906
Reian Beltran <rmb20j@fsu.edu>
2022-10-18 20:39:45 -0400

    added yellow circles

--- sketch.js
+++ sketch.js
@@ -1,7 +1,12 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(500, 500);
 }
 
 function draw() {
-  background(125);
+  background(119, 0, 200);
+  fill(252, 266, 5);
+  circle(0, 250, 250);
+  circle(250, 0, 250);
+  circle(250, 500, 250);
+  circle(500, 250, 250);
 }
