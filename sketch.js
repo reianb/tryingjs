532b94dea8f134c44ddbfbcee6378ef17142b50b
Reian Beltran <rmb20j@fsu.edu>
2022-10-18 21:00:22 -0400

    added animation to circles

--- sketch.js
+++ sketch.js
@@ -1,3 +1,5 @@
+let d = 250;
+
 function setup() {
   createCanvas(500, 500);
 }
@@ -5,8 +7,13 @@ function setup() {
 function draw() {
   background(119, 0, 200);
   fill(252, 266, 5);
-  circle(0, 250, 250);
-  circle(250, 0, 250);
-  circle(250, 500, 250);
-  circle(500, 250, 250);
+  circle(0, 250, d);
+  circle(250, 0, d);
+  circle(250, 500, d);
+  circle(500, 250, d);
+  d++;
+
+  if (d > 500) {
+    d = 250;
+  }
 }
