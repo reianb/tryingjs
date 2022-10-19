6159349e2755eadffd4e039848aae00da9e3fd5d
Reian Beltran <rmb20j@fsu.edu>
2022-10-18 21:13:13 -0400

    added purple circles

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 let d = 250;
-
+let dtwo = d/2;
 function setup() {
   createCanvas(500, 500);
 }
@@ -7,6 +7,7 @@ function setup() {
 function draw() {
   background(119, 0, 200);
   fill(252, 266, 5);
+  noStroke();
   circle(0, 250, d);
   circle(250, 0, d);
   circle(250, 500, d);
@@ -16,4 +17,12 @@ function draw() {
   if (d > 500) {
     d = 250;
   }
+
+  fill(119, 0, 200);
+  noStroke;
+  circle(250, 250, dtwo);
+  circle(500/4, 375, dtwo);
+  circle(375, 375, dtwo);
+  circle(500/4, 500/4, dtwo);
+  circle(375, 500/4, dtwo);
 }
